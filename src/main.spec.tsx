37d51bcb551d8bfbf4fd546b561780b36b29427e
cieldon32 <cieldon32@gmail.com>
2020-06-14 10:51:53 +0800

    init

---
+++ src/main.spec.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { screen } from '@testing-library/dom'
+import {useSubscription} from './main';
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "set":
+      return action.count;
+    default:
+      return state;
+  }
+};
+
+const Counter = ({ count, dispatch }) => (
+  <div>
+    {count}
+    <button onClick={() => dispatch({ type: "increment" })}>+1</button>
+    <button onClick={() => dispatch({ type: "decrement" })}>-1</button>
+    <button onClick={() => dispatch({ type: "set", count: 0 })}>reset</button>
+  </div>
+);
+
+const useCount1 = useSubscription(reducer, initialState);
+const useCount2 = useSubscription(reducer, initialState);
+
+const Counter1 = () => {
+  const [count, dispatch] = useCount1();
+  return <Counter count={count} dispatch={dispatch} />;
+};
+
+const Counter2 = () => {
+  const [count, dispatch] = useCount2();
+  return <Counter count={count} dispatch={dispatch} />;
+};
+
+const ExampleCounter = () => (
+  <>
+    <Counter1 data-testid="counter1a" />
+    <Counter1 data-testid="counter1b" />
+    <Counter2 data-testid="counter2a" />
+    <Counter2 data-testid="counter2b" />
+  </>
+);
+
+
+describe("check hook", () => {
+  it("example: counter", async function () {
+    const {findByTestId, getByTestId, getByText, queryAllByText} = render(
+      <ExampleCounter />
+    );
+    
+    // const counter1a = getByTestId('counter1a');
+    // const increame = fireEvent(
+    //   queryAllByText('+1'),
+    //   new MouseEvent('click', {
+    //     bubbles: true,
+    //     cancelable: true,
+    //     button: 0
+    //   })
+    // )
+
+    expect(3).toBe(3);
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import React, { useEffect, useReducer } from "react";
+
+const useForceUpdate = () => useReducer((state) => !state, false)[1];
+
+export const useSubscription = (reducer, initialState) => {
+  const subscribers = [];
+  let state = initialState;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    subscribers.forEach((callback) => callback());
+  };
+  const useSharedState = () => {
+    const forceUpdate = useForceUpdate();
+    useEffect(() => {
+      const callback = () => forceUpdate();
+      subscribers.push(callback);
+      callback(); // in case it's already updated
+      const cleanup = () => {
+        const index = subscribers.indexOf(callback);
+        subscribers.splice(index, 1);
+      };
+      return cleanup;
+    }, []);
+    return [state, dispatch];
+  };
+  return useSharedState;
+};
